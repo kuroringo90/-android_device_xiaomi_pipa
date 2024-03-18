c0190126a5c30a0b5b76cd8aa820577e2246389e
kuroringo90 <ak4700@gmx.com>
2024-03-18 08:27:04 +0100

    pipa: Import QDCM display calibration from hentaiOS
    I am not certain if the exact author to be attributed is @ Dyneteve,
    as the file was extracted by @ KakatkarAkshay from apollo hentaiOS image,
    but they did not release its device tree code.
    
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: Idc690f696656d209b0623b4a39cf09314946f7f8
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Keyboard
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/Xiaomi_Keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Xiaomi_Keyboard.idc
