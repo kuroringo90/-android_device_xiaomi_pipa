117b4f8098664b662266e30b8e20e9b032ebfd71
kuroringo90 <ak4700@gmx.com>
2024-04-26 13:13:01 +0200

    pipa: update rising bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rising_pipa.mk
+    $(LOCAL_DIR)/lineage_pipa.mk
 
 COMMON_LUNCH_CHOICES := \
-    rising_pipa-user \
-    rising_pipa-userdebug \
-    rising_pipa-eng
+    lineage_pipa-user \
+    lineage_pipa-userdebug \
+    lineage_pipa-eng
